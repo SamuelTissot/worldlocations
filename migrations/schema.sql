1da5365a8ceaaa00f4dcba39b4de0ded49fedd89
SamuelTissot <tissotjobin@gmail.com>
2019-02-08 08:23:21 -0500

    Database Migrations
    - subdivision_codes
    - subdivision_names
    
    Signed-off-by: SamuelTissot <tissotjobin@gmail.com>

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.24, for osx10.14 (x86_64)
 --
--- Host: localhost    Database: worldlocations_test
+-- Host: localhost    Database: worldlocations_development
 -- ------------------------------------------------------
 -- Server version	5.7.24
 
@@ -23,16 +23,34 @@ DROP TABLE IF EXISTS `country_codes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `country_codes` (
-                               `alpha_2_code`       varchar(2) NOT NULL,
-                               `alpha_3_code`       varchar(3)          DEFAULT NULL,
-                               `numeric_code`       int(11)             DEFAULT NULL,
-                               `international_name` varchar(255)        DEFAULT NULL,
-                               `is_independant`     tinyint(4)          DEFAULT NULL,
-                               `iso_status`         varchar(25)         DEFAULT NULL,
-                               `created_at`         datetime   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                               `updated_at`         datetime   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                               `alpha_2_code`       VARCHAR(2) NOT NULL,
+                               `alpha_3_code`       VARCHAR(3)          DEFAULT NULL,
+                               `numeric_code`       INT(11)             DEFAULT NULL,
+                               `international_name` VARCHAR(255)        DEFAULT NULL,
+                               `is_independant`     TINYINT(4)          DEFAULT NULL,
+                               `iso_status`         VARCHAR(25)         DEFAULT NULL,
+                               `created_at`         DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                               `updated_at`         DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP,
                                PRIMARY KEY (`alpha_2_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `language_codes`
+--
+
+DROP TABLE IF EXISTS `language_codes`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `language_codes`
+(
+  `language_alpha_2_code` VARCHAR(2) NOT NULL,
+  `language_alpha_3_code` VARCHAR(3)          DEFAULT NULL,
+  `created_at`            DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at`            DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`language_alpha_2_code`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -57,15 +75,39 @@ DROP TABLE IF EXISTS `subdivision_codes`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `subdivision_codes`
 (
-  `subdivision_code`   varchar(6) NOT NULL,
-  `alpha_2_code`       varchar(2)          DEFAULT NULL,
-  `international_name` varchar(255)        DEFAULT NULL,
-  `category`           varchar(50)         DEFAULT NULL,
-  `created_at`         datetime   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at`         datetime   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `subdivision_code`   VARCHAR(6) NOT NULL,
+  `alpha_2_code`       VARCHAR(2)          DEFAULT NULL,
+  `international_name` VARCHAR(255)        DEFAULT NULL,
+  `category`           VARCHAR(50)         DEFAULT NULL,
+  `created_at`         DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at`         DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`subdivision_code`),
   KEY `alpha_2_code` (`alpha_2_code`),
   CONSTRAINT `subdivision_codes_ibfk_1` FOREIGN KEY (`alpha_2_code`) REFERENCES `country_codes` (`alpha_2_code`) ON DELETE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `subdivision_names`
+--
+
+DROP TABLE IF EXISTS `subdivision_names`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subdivision_names`
+(
+  `subdivision_code`      VARCHAR(6)   NOT NULL,
+  `language_alpha_2_code` VARCHAR(2)   NOT NULL,
+  `name`                  VARCHAR(255) NOT NULL,
+  `local_variation`       VARCHAR(255)          DEFAULT NULL,
+  `created_at`            DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at`            DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`subdivision_code`, `language_alpha_2_code`),
+  KEY `language_alpha_2_code` (`language_alpha_2_code`),
+  KEY `subdivision_names_subdivision_code_language_alpha_2_code_idx` (`subdivision_code`, `language_alpha_2_code`),
+  CONSTRAINT `subdivision_names_ibfk_1` FOREIGN KEY (`subdivision_code`) REFERENCES `subdivision_codes` (`subdivision_code`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `subdivision_names_ibfk_2` FOREIGN KEY (`language_alpha_2_code`) REFERENCES `language_codes` (`language_alpha_2_code`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -79,4 +121,4 @@ CREATE TABLE `subdivision_codes`
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-02-07  8:03:12
+-- Dump completed on 2019-02-08  8:18:09
